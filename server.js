4eb75aaa3cd83f4e04c7e0ce0d35678afaf74613
NotaFive5 <87462234+NotaFive5@users.noreply.github.com>
2025-02-21 00:21:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Раздача статических файлов
-app.use(express.static(path.join(__dirname)));
+// Настройка статической директории
+app.use(express.static(path.join(__dirname, '.')));
 
-// Ручка для главной страницы
+// Отдача index.html при обращении к корневому URL
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Запуск сервера
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
